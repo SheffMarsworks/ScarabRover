24a733a798d44b2a875bf5c45da8bc31c914a715
Renzo Damian <renzo.damian.go@gmail.com>
2025-04-11 17:50:46 +0100

    Add Mars world

--- src/rover_description/launch/gazebo.launch.py
+++ src/rover_description/launch/gazebo.launch.py
@@ -14,12 +14,13 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
-world_file = 'warehouse.sdf' # warehouse
+world_file = "mars.world.sdf"  # warehouse
 
 
 def generate_launch_description():
     rover_description = get_package_share_directory("rover_description")
     # world_file = os.path.join(rover_description, "worlds", "wro_world.sdf")
+
     model_arg = DeclareLaunchArgument(
         name="model",
         default_value=os.path.join(rover_description, "urdf", "rover.urdf.xacro"),
@@ -27,9 +28,10 @@ def generate_launch_description():
     )
 
     simu_time = DeclareLaunchArgument(
-        'use_sim_time',
-        default_value='True',
-        description='Use simulation (Gazebo) clock if true')
+        "use_sim_time",
+        default_value="True",
+        description="Use simulation (Gazebo) clock if true",
+    )
     qt_qpa_platform = SetEnvironmentVariable(name="QT_QPA_PLATFORM", value="xcb")
 
     gazebo_resource_path = SetEnvironmentVariable(
@@ -40,13 +42,11 @@ def generate_launch_description():
     ros_distro = os.environ["ROS_DISTRO"]
     is_ignition = "True" if ros_distro == "humble" else "False"
 
-    robot_description = ParameterValue(Command([
-            "xacro ",
-            LaunchConfiguration("model"),
-            " is_ignition:=",
-            is_ignition
-        ]),
-        value_type=str
+    robot_description = ParameterValue(
+        Command(
+            ["xacro ", LaunchConfiguration("model"), " is_ignition:=", is_ignition]
+        ),
+        value_type=str,
     )
 
     robot_state_publisher_node = Node(
@@ -63,8 +63,8 @@ def generate_launch_description():
             ]
         ),
         launch_arguments=[
-                ('gz_args', [rover_description + "/worlds/" + world_file, ' -v 4', ' -r'])
-        ]
+            ("gz_args", [rover_description + "/worlds/" + world_file, " -v 4", " -r"])
+        ],
     )
 
     gz_spawn_entity = Node(
@@ -80,6 +80,8 @@ def generate_launch_description():
             "0.0",  # X position in meters
             "-y",
             "-1.0",  # Y position in meters
+            "-z",
+            "0.15", # Z position in meters
         ],
     )
 
@@ -90,11 +92,11 @@ def generate_launch_description():
             "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
             "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU",
             "/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan",
-            '/camera/image_raw@sensor_msgs/msg/Image@ignition.msgs.Image',
-            '/camera/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo',
-            '/depth_camera/points@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloudPacked',
-            '/depth_camera/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo',
-            '/depth_camera/image@sensor_msgs/msg/Image@ignition.msgs.Image',
+            "/camera/image_raw@sensor_msgs/msg/Image@ignition.msgs.Image",
+            "/camera/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo",
+            "/depth_camera/points@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloudPacked",
+            "/depth_camera/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo",
+            "/depth_camera/image@sensor_msgs/msg/Image@ignition.msgs.Image",
         ],
         remappings=[
             ("/imu", "/imu/out"),
@@ -103,6 +105,21 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
+            SetEnvironmentVariable(
+                name="GZ_SIM_RESOURCE_PATH",
+                value=os.path.join(rover_description, "models")
+                + ":"
+                + os.environ.get("GZ_SIM_RESOURCE_PATH", ""),
+            ),
+            SetEnvironmentVariable(
+                name="GAZEBO_MODEL_PATH",
+                value="/home/damian/marsworks_scarab_ws/src/rover_description/models:"
+                + os.environ.get("GAZEBO_MODEL_PATH", ""),
+            ),
+            SetEnvironmentVariable(
+                name="IGN_GAZEBO_RESOURCE_PATH",
+                value="/home/damian/marsworks_scarab_ws/src/rover_description/models",
+            ),
             simu_time,
             qt_qpa_platform,
             model_arg,
