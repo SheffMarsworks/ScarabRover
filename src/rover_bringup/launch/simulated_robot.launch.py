9023334d2bd9e10f42099c4af3d19ef06761bb08
Renzo Damian <renzo.damian.go@gmail.com>
2025-04-18 17:02:37 +0100

    Add keyboard controller

--- src/rover_bringup/launch/simulated_robot.launch.py
+++ src/rover_bringup/launch/simulated_robot.launch.py
@@ -1,44 +1,67 @@
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    gazebo = IncludeLaunchDescription(
-        os.path.join(
-            get_package_share_directory("rover_description"),
-            "launch",
-            "gazebo.launch.py",
-        ),
+    # Declare the teleop argument with an empty default value (which will be treated as "none")
+    teleop_arg = DeclareLaunchArgument(
+        'teleop',
+        default_value='',
+        description='Type of teleop: joystick, keyboard, or none'
     )
 
-    controller = IncludeLaunchDescription(
-        os.path.join(
-            get_package_share_directory("rover_controller"),
-            "launch",
-            "controller.launch.py"
-        ),
-        launch_arguments={
-            "use_python": "False"
-        }.items(),
-    )
+    def launch_setup(context, *args, **kwargs):
+        teleop = LaunchConfiguration('teleop').perform(context)
 
-    joystick = IncludeLaunchDescription(
-        os.path.join(
-            get_package_share_directory("rover_controller"),
-            "launch",
-            "joystick_teleop.launch.py"
-        ),
-        launch_arguments={
-            "use_sim_time": "True"
-        }.items()
-    )
+        gazebo = IncludeLaunchDescription(
+            os.path.join(
+                get_package_share_directory("rover_description"),
+                "launch",
+                "gazebo.launch.py",
+            ),
+        )
 
-    return LaunchDescription(
-        [
-            gazebo,
-            controller,
-            joystick,
-        ]
-    )
+        controller = IncludeLaunchDescription(
+            os.path.join(
+                get_package_share_directory("rover_controller"),
+                "launch",
+                "controller.launch.py"
+            ),
+            launch_arguments={
+                "use_python": "False"
+            }.items(),
+        )
+
+        launch_descriptions = [gazebo, controller]
+
+        if teleop == 'joystick':
+            joystick = IncludeLaunchDescription(
+                os.path.join(
+                    get_package_share_directory("rover_controller"),
+                    "launch",
+                    "joystick_teleop.launch.py"
+                ),
+                launch_arguments={
+                    "use_sim_time": "True"
+                }.items()
+            )
+            launch_descriptions.append(joystick)
+        elif teleop == 'keyboard':
+            keyboard = IncludeLaunchDescription(
+                os.path.join(
+                    get_package_share_directory("rover_controller"),
+                    "launch",
+                    "keyboard_teleop.launch.py"
+                )
+            )
+            launch_descriptions.append(keyboard)
+
+        return launch_descriptions
+
+    return LaunchDescription([
+        teleop_arg,
+        OpaqueFunction(function=launch_setup)
+    ])
---
+++ src/rover_controller/launch/keyboard_teleop.launch.py
@@ -0,0 +1,30 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation time if true'
+    )
+    
+    keyboard_teleop_node = Node(
+        package='teleop_twist_keyboard',
+        executable='teleop_twist_keyboard',
+        name='teleop_keyboard',
+        output='screen',
+        prefix='xterm -e', 
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
+        ],
+    )
+
+    return LaunchDescription([
+        use_sim_time_arg,
+        keyboard_teleop_node,
+    ])
